0dc7bfdd0581fd5971249bceb5870521adaa34fe
ThomasNam <red@hanisoft.kr>
2025-08-24 13:14:25 +0900

    시작 페이지 메인 컴포넌트까지 적용

---
+++ components/main-header/main-header.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+
+const MainHeader = () => {
+    return (
+        <>
+            <MainHeaderBackground/>
+            <header className={classes.header}>
+                <Link href="/public" className={classes.logo}>
+                    <Image src={logoImg} alt='logo' priority/>
+                    NextLevel Food
+                </Link>
+
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Browse Meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Foodies Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+};
+
+export default MainHeader;
